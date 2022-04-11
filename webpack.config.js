b2626f3ee3287bd10f751779b3362f9c6a6fbc6b
Igor Karpov <karpov.zgr@mail.ru>
2022-04-11 17:51:18 +0300

    webpack config check

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash].js',
   },
+  devServer: {
+    port: 3000
+  },
   plugins: [
     new HTMLWebpackPlugin({template: './src/index.html'}),
     new CleanWebpackPlugin()
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        use: ['file-loader'],
+      }
+    ]
+  }
 }
