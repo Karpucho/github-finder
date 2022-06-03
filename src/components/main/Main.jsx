3cf1a6aacfef9874e7a7bee4fa63d4e8475d9d88
Karpucho <karpov.zgr@mail.ru>
2022-06-03 22:35:42 +0300

    test dev

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentPage } from '../../reducers/reposReducer';
 import { createPages } from '../../utils/pagesCreator';
